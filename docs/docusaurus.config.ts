54932834da6846a2d6f3725de500c9f612bde605
unknown <durgeshdwivedi81@gmail.com>
2025-08-24 22:00:11 +0530

    fix(docs): update production url [skip ci]

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://2004durgesh.github.io/react-native-media-toolkit/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/react-native-media-toolkit/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
