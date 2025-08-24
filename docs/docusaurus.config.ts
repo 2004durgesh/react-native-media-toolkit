d67d9e7ae136f83bb6728ffeb60b1315f108e1b2
unknown <durgeshdwivedi81@gmail.com>
2025-08-24 23:10:20 +0530

    fix(ci): update ci.yml to ignore docs dir and rename deploy.yml to docs.yml

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://2004durgesh.github.io/react-native-media-toolkit/',
+  url: 'https://2004durgesh.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-native-media-toolkit/',
